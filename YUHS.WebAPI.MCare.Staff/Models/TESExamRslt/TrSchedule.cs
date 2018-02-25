037cb38a064c20c20bdb0f25aa7542001e666a17
Kyunam Kevin Kwak <qeist@hanmail.net>
2018-02-25 08:44:04 +0000

    Update TrSchedule.cs

--- YUHS.WebAPI.MCare.Staff/Models/TESExamRslt/TrSchedule.cs
+++ YUHS.WebAPI.MCare.Staff/Models/TESExamRslt/TrSchedule.cs
@@ -1,6 +1,6 @@
 namespace YUHS.WebAPI.MCare.Staff.Models.TESExamRslt
 {
-    public class TrSchedule
+    public class ExamRsltByEx
     {
         public string Rslt { get; set; }
         public string ReadDrNm { get; set; }
