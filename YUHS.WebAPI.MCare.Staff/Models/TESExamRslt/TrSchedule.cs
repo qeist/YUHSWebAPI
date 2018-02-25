d666896ea948d77dc0e7d3a2be771cfff9153118
Kyunam Kevin Kwak <qeist@hanmail.net>
2018-02-25 23:39:49 +0000

    모델 추가) 시술 진행상황 조회
    모델 추가) 시술 진행상황 조회

---
+++ YUHS.WebAPI.MCare.Staff/Models/TESExamRslt/TrSchedule.cs
@@ -0,0 +1,27 @@
+namespace YUHS.WebAPI.MCare.Staff.Models.TESExamRslt
+{
+    public class TrSchedule
+    {
+        public string hospitalCd { get; set; }
+        public string treatmentDt { get; set; }
+        public string departmentNm { get; set; }
+        public string emergencyYn { get; set; }
+        public string treatmentFmDt { get; set; }
+        public string treatmentFmTm { get; set; }
+        public string treatmentToDt { get; set; }
+        public string treatmentToTm { get; set; }
+        public string treatmentDrNm { get; set; }
+        public string treatmentCntr { get; set; }
+        public string patientId { get; set; }
+        public string patientNm { get; set; }
+        public string sex { get; set; }
+        public string age { get; set; }
+        public string birthDt { get; set; }
+        public string cellphone { get; set; }
+        public string ward { get; set; }
+        public string treatmentNm { get; set; }
+        public string diagnosisNm { get; set; }
+        public string treatmentTyp { get; set; }
+
+    }
+}
