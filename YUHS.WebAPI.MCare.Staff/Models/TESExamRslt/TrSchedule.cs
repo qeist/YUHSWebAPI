67b5f15cefddc1bf655bc5c69fd7ffb88c366d5f
Kyunam Kevin Kwak <qeist@hanmail.net>
2018-02-25 08:28:28 +0000

    모델 추가) 기능검사 판독 내용 조회
    모델 추가) 기능검사 판독 내용 조회

---
+++ YUHS.WebAPI.MCare.Staff/Models/TESExamRslt/TrSchedule.cs
@@ -0,0 +1,9 @@
+namespace YUHS.WebAPI.MCare.Staff.Models.TESExamRslt
+{
+    public class TrSchedule
+    {
+        public string Rslt { get; set; }
+        public string ReadDrNm { get; set; }
+        public string SplstDrNm { get; set; }
+    }
+}
